f28839ce00acaed6c4eb4812540661e4c904ffe0
dubilyer <eduard.dubilyer@finastra.com>
2020-10-08 12:21:02 +0300

    init

--- src/main/java/map_reduce/functional/ConsumableFunction.java
+++ src/main/java/map_reduce/functional/ConsumableFunction.java
@@ -1,6 +1,5 @@
 package map_reduce.functional;
 
-import java.util.Objects;
 import java.util.function.Consumer;
 
 @FunctionalInterface
@@ -12,7 +11,6 @@ public interface ConsumableFunction<T, R> {
     R apply(T t);
 
     default <V> ConsumableFunction<T, V> andThen(ConsumableFunction<? super R, ? extends V> after) {
-        Objects.requireNonNull(after);
         return (T t) -> after.apply(apply(t));
     }
 }
